fe96c0871b8e31b93d149c670ae43f13e70ae01e
wstagf <wstagf@gmail.com>
2016-01-07 16:48:05 -0300

    Create homeContent.php

---
+++ homeContent.php
@@ -0,0 +1,29 @@
+<!-- /.row -->
+<div class="row" style="padding: 10px 0 0 0">
+	<div class="col-lg-3 col-md-6">
+		coluna 3
+	</div>
+	<div class="col-lg-3 col-md-6">
+		coluna 3
+	</div>
+	<div class="col-lg-3 col-md-6">
+		coluna 3
+	</div>
+	<div class="col-lg-3 col-md-6">
+		coluna 3
+	</div>
+</div>
+<!-- /.row -->
+
+<!-- /.row -->
+<div class="row">
+	<div class="col-lg-8">
+		coluna 8
+	</div>
+	<!-- /.col-lg-8 -->
+	<div class="col-lg-4">
+		coluna 4
+	</div>
+	<!-- /.col-lg-4 -->
+</div>
+<!-- /.row -->
