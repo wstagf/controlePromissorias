3844c027e7bf9da3c520ccfc6ab4120b5322009f
wstagf <wstagf@gmail.com>
2016-01-11 10:52:19 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -404,7 +404,113 @@ $app->get('/excluirEndereco/:idEndereco', 'auth', function ($idEndereco) use ($a
 		 mysql_close($link);
     }
 );
-// fim Crud  Perfil Usuario
+// fim Crud  endereco
+
+
+
+
+
+// Crud Pais
+$app->post(
+    '/createPais',
+    function () use ($app) {
+		$data = json_decode($app->request()->getBody());
+        $descricao = (isset($data->descricao)) ? $data->descricao : "";
+
+        $link =createDB();
+       
+		$sql = "INSERT INTO pais (descricao) VALUES ('".$descricao."');";
+		$result =  mysql_query($sql, $link);
+        if (mysql_errno($link) > 0 ) {
+			echo json_encode(array("erro"=>true, "mysql_errno" => mysql_errno($link), "mysql_error" => mysql_error($link), "sql" => $sql));
+        } else {
+            if ($result) {
+                echo json_encode(array("erro"=>false, "descricao"=>$descricao, "sql" => $sql));
+            } 
+        }
+		 mysql_close($link);
+    }
+);
+// READ - 01: Lista Completa
+$app->get('/listarPaises', 'auth', function () use ($app) {
+		$link = createDB();
+		$sql = "select pais.id, pais.descricao from pais order by pais.id";
+		$result =  mysql_query($sql, $link);
+        if (mysql_errno($link) > 0 ) {
+			echo json_encode(array("erro"=>true, "mysql_errno" => mysql_errno($link), "mysql_error" => mysql_error($link), "sql" => $sql));
+        } else {
+			$rows = array();
+			while ($row = mysql_fetch_array($result, MYSQL_BOTH))
+			{
+				$rows[] = $row;
+			}
+			echo json_encode(array("erro"=>"false", "result"=>$rows ));
+        }
+		 mysql_close($link);
+    }
+);
+// READ - 02: item unico
+$app->get('/getPais/:idPais', 'auth', function ($idPais) use ($app) {
+		$idPais = (int)$idPais;
+		$link = createDB();
+		$sql = "select pais.id, pais.descricao from pais  where pais.id = ".$idPais.";";
+		$result =  mysql_query($sql, $link);
+        if (mysql_errno($link) > 0 ) {
+			echo json_encode(array("erro"=>true, "mysql_errno" => mysql_errno($link), "mysql_error" => mysql_error($link), "sql" => $sql));
+        } else {
+			$rows = array();
+			while ($row = mysql_fetch_array($result, MYSQL_BOTH))
+			{
+				$rows[] = $row;
+			}
+			echo json_encode(array("erro"=>"false", "result"=>$rows[0] ));
+        }
+		 mysql_close($link);
+    }
+);
+// Update
+$app->post('/alterarPais/:idPais', 'auth', function ($idPais) use ($app) {
+        
+        $data = json_decode($app->request()->getBody());
+        $idPais = (int)$idPais;
+        $descricao = (isset($data->descricao)) ? $data->descricao : "";
+       
+		$link =createDB();
+        
+		$sql = "UPDATE pais  SET  descricao = '".$descricao."' WHERE  id = ".$idPais.";";
+		$result =  mysql_query($sql, $link);
+        if (mysql_errno($link) > 0 ) {
+			echo json_encode(array("erro"=>true, "mysql_errno" => mysql_errno($link), "mysql_error" => mysql_error($link), "sql" => $sql));
+        } else {
+            if ($result) {
+                echo json_encode(array("erro"=>false));
+            } 
+        }
+		 mysql_close($link);
+        
+    }
+);
+// Delete
+$app->get('/excluirPais/:idPais', 'auth', function ($idPais) use ($app) {       
+		$idPais = (int)$idPais;
+        $link =createDB();
+        
+        $sql = "DELETE FROM pais WHERE id = ".$idPais.";";
+		$result =  mysql_query($sql, $link);
+        if (mysql_errno($link) > 0 ) {
+			echo json_encode(array("erro"=>true, "mysql_errno" => mysql_errno($link), "mysql_error" => mysql_error($link), "sql" => $sql));
+        } else {
+            if ($result) {
+                echo json_encode(array("erro"=>false));
+            } 
+        }
+		 mysql_close($link);
+    }
+);
+// fim Crud   Pais
+
+
+
 
 
 
