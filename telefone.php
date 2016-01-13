976c927d7e1e4f412174b4719b13b976a920418f
wstagf <wstagf@gmail.com>
2016-01-13 14:00:06 -0300

    Update telefone.php

--- telefone.php
+++ telefone.php
@@ -28,6 +28,7 @@
                             <thead>
                                 <tr>
                                     <th>ID</th>
+									<th>Pessoa</th>
                                     <th>DDD</th>
 									<th>Número Telefone</th>
 									<th>Ramal</th>
@@ -38,6 +39,7 @@
                             <tbody>
                                 <tr class="odd gradeX" ng-repeat="model in listaTelefones track by model.id">
                                     <td>{{model.id}}</td>
+									<td>{{model.pessoaName}}</td>
                                     <td>{{model.ddd}}</td>
                                     <td>{{model.numero}}</td>
 									<td>{{model.ramal}}</td>
@@ -55,94 +57,40 @@
                     <div class="dataTable_wrapper">
 						<form class="form-horizontal" ng-submit="inserirTelefone()">
 							<div class="form-group">
-								<div class="col-sm-6">
-									<label for="inputEmail3" class="control-label">Nome*</label>
-									<input type="text" class="form-control" id="inputAdd_nome" placeholder="Nome da Telefone " ng-model="TelefoneAdd.descricao" required>
-								</div>
-								<div class="col-sm-6">
-									<div class="col-sm-4">
-										<label for="inputEmail3" class="control-label">Telefone</label>
-										<input type="text" class="form-control" id="inputAdd_dddTelefone" placeholder="(0XX)" ng-model="TelefoneAdd.dddtelefone" >
-									</div>
-									<div class="col-sm-8">
-										<label for="inputEmail3" class="control-label">&nbsp</label>
-										<input type="text" class="form-control" id="inputAdd_numeroTelefone" placeholder="99999-9999" ng-model="TelefoneAdd.numtelefone" >
-									</div>
-								</div>
-							</div>
-							<div class="form-group">
-								<div class="col-sm-6">
-									<label for="inputEmail3" class="control-label">Observações*</label>
-									<input type="text" class="form-control" id="inputAdd_observacoes" placeholder="Observações úteis" ng-model="TelefoneAdd.observacoes" required>
-								</div>
-							</div>
-							<div class="well">
-								<h4>Endereço</h4>
-								
-								<div class="form-group">
-								<div class="col-sm-6">
-									<label for="inputEmail3" class="control-label">Logradouro*</label>
-									<input type="text" class="form-control" id="inputAdd_logradouro" placeholder="Rua / Avenida ... " ng-model="TelefoneAdd.enderecoLogradouro" required>
+								<div class="col-sm-3">
+									<label for="inputEmail3" class="control-label">DDD</label>
+									<input type="text" class="form-control" id="input_dddTelefone" placeholder="DDD" ng-model="TelefoneAdd.ddd" >
 								</div>
 								<div class="col-sm-4">
-									<label for="inputEmail3" class="control-label">Complemento</label>
-									<input type="text" class="form-control" id="inputAdd_complemento" placeholder="Qd. XX, Lt. YYY, Apt 0..." ng-model="TelefoneAdd.enderecoComplemento" >
-								</div>
-								<div class="col-sm-2">
-									<label for="inputEmail3" class="control-label">Número</label>
-									<input type="text" class="form-control" id="inputAdd_numero" placeholder="Usuário" ng-model="TelefoneAdd.enderecoNumero" >
-								</div>
-							</div>
-							<div class="form-group">
-								<div class="col-sm-6">
-									<label for="inputEmail3" class="control-label">Bairro*</label>
-									<input type="text" class="form-control" id="inputAdd_bairro" placeholder="Bairro / Setor" ng-model="TelefoneAdd.enderecoBairro" required>
+									<label for="inputEmail3" class="control-label">Telefone</label>
+									<input type="text" class="form-control" id="input_numeroTelefone" placeholder="Número do Telefone" ng-model="TelefoneAdd.numero" >
 								</div>
 								<div class="col-sm-4">
-									<label for="inputEmail3" class="control-label">Cidade*</label>
-									<select ng-model="TelefoneAdd.enderecoCidadeId"  class="form-control">
-									       <option ng-repeat="cidade in listaCidades" value="{{cidade.id}}" > {{cidade.descricao}} </option>
-								    </select>
-								</div>
-								<div class="col-sm-2">
-									<label for="inputEmail3" class="control-label">Cep</label>
-									<input type="text" class="form-control" id="inputAdd_cep" placeholder="Cep" ng-model="TelefoneAdd.enderecoCep">
+									<label for="inputEmail3" class="control-label">Ramal</label>
+									<input type="text" class="form-control" id="input_numeroTelefone" placeholder="Ramal do Telefone" ng-model="TelefoneAdd.ramal" >
 								</div>
 							</div>
-							
 							<div class="form-group">
 								<div class="col-sm-6">
-								<label for="inputEmail3" class="control-label">Ponto de Referencia</label>
-									<input type="text" class="form-control" id="inputAdd_ponto_referencia" placeholder="Ponto de Referencia" ng-model="TelefoneAdd.enderecoPontoReferencia" >
+									<label for="inputEmail3" class="control-label">Observações</label>
+									<input type="text" class="form-control" id="input_numeroTelefone" placeholder="Observação" ng-model="TelefoneAdd.observacao" >
 								</div>
-														
-								<div class="col-sm-3">
-									<label for="inputEmail3" class="control-label">Longetude</label>
-									<input type="text" class="form-control" id="inputAdd_longetude" placeholder="Longetude" ng-model="TelefoneAdd.enderecoLongetude" >
-								</div>
-														
-								<div class="col-sm-3">
-									<label for="inputEmail3" class="control-label">Latitude</label>
-									<input type="text" class="form-control" id="inputAdd_latitude" placeholder="Latitude" ng-model="TelefoneAdd.enderecoLatitude" >
+								<div class="col-sm-6">
+									<label for="inputEmail3" class="control-label">Pessoa</label>
+									<select ng-model="TelefoneAdd.pessoa_id"  class="form-control">
+									<option ng-repeat="pessoa in listaPessoas" value="{{pessoa.id}}" > {{pessoa.razao_social}} </option>
+									</select>
 								</div>
 							</div>
 
 							<div class="form-group">
-							<div class="col-sm-9">
+								<div class="col-sm-2">
 								
+								</div>
+								<div class="col-sm-10">
+									<button type="submit" class="btn btn-default">Cadastrar Telefone</button>
+								</div>
 							</div>
-							
-							</div>
-							</div>
-							<div class="col-sm-10">
-								<button type="submit" class="btn btn-default">Cadastrar Telefone</button>
-							</div>
-							
-							                                    
-                                    <td>{{model.ddd}}</td>
-                                    <td>{{model.numero}}</td>
-									<td>{{model.ramal}}</td>
-									<td>{{model.observacao}}</td>
 						</form>
                     </div>
                 </div>
@@ -150,28 +98,34 @@
                     <div class="dataTable_wrapper">
 						<form class="form-horizontal" ng-submit="alterarTelefone()">
 							<div class="form-group">
-								<div class="col-sm-12">
-									<div class="col-sm-2">
-										<label for="inputEmail3" class="control-label">DDD</label>
-										<input type="text" class="form-control" id="input_dddTelefone" placeholder="DDD" ng-model="Telefone.ddd" >
-									</div>
-									<div class="col-sm-4">
-										<label for="inputEmail3" class="control-label">Telefone</label>
-										<input type="text" class="form-control" id="input_numeroTelefone" placeholder="Número do Telefone" ng-model="Telefone.numero" >
-									</div>
-									<div class="col-sm-2">
-										<label for="inputEmail3" class="control-label">Ramal</label>
-										<input type="text" class="form-control" id="input_numeroTelefone" placeholder="Ramal do Telefone" ng-model="Telefone.ramal" >
-									</div>
-									<div class="col-sm-4">
-										<label for="inputEmail3" class="control-label">Observações</label>
-										<input type="text" class="form-control" id="input_numeroTelefone" placeholder="Observação" ng-model="Telefone.observacao" >
-									</div>
+								<div class="col-sm-3">
+									<label for="inputEmail3" class="control-label">DDD</label>
+									<input type="text" class="form-control" id="input_dddTelefone" placeholder="DDD" ng-model="Telefone.ddd" >
+								</div>
+								<div class="col-sm-4">
+									<label for="inputEmail3" class="control-label">Telefone</label>
+									<input type="text" class="form-control" id="input_numeroTelefone" placeholder="Número do Telefone" ng-model="Telefone.numero" >
+								</div>
+								<div class="col-sm-4">
+									<label for="inputEmail3" class="control-label">Ramal</label>
+									<input type="text" class="form-control" id="input_numeroTelefone" placeholder="Ramal do Telefone" ng-model="Telefone.ramal" >
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-6">
+									<label for="inputEmail3" class="control-label">Observações</label>
+									<input type="text" class="form-control" id="input_numeroTelefone" placeholder="Observação" ng-model="Telefone.observacao" >
+								</div>
+								<div class="col-sm-6">
+									<label for="inputEmail3" class="control-label">Pessoa</label>
+									<select ng-model="Telefone.pessoa_id"  class="form-control">
+									<option ng-repeat="pessoa in listaPessoas" value="{{pessoa.id}}" > {{pessoa.razao_social}} </option>
+									</select>
 								</div>
 							</div>
 							
 							<div class="form-group">
-								<div class="col-sm-9">
+								<div class="col-sm-2">
 								
 								</div>
 								<div class="col-sm-10">
